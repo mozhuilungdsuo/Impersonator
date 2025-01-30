03823bf657030c35590e131e9881576434b9665d
Nerdy Naga <mozhui.lungdsuo@gmail.com>
2025-01-30 11:52:46 +0530

    aa

--- src/database/migrations/2023_10_01_000000_create_impersonation_logs_table.php
+++ src/database/migrations/2023_10_01_000000_create_impersonation_logs_table.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImpersonationLogsTable extends Migration
+class CreateImpersonationLogTable extends Migration
 {
     public function up()
     {
