2e83b9106a1573d1fec9caf37dddf8e2dde397ab
Nerdy Naga <mozhui.lungdsuo@gmail.com>
2025-02-04 11:36:11 +0530

    aa

--- resources/ stop-impersonate.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-@if(session()->has('impersonator'))
-    <div style="position: fixed; bottom: 20px; right: 20px;">
-        <a href="{{ route('impersonate.stop') }}" style="background: red; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none;">
-            Stop Impersonation
-        </a>
-    </div>
-@endif
--- resources/impersonate.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ route('impersonate.start', ['id' => $userId]) }}"
-    style="background: blue; color: white; padding: 10px 20px; border-radius: 5px; text-decoration: none;">
-    Impersonate User
-</a>
