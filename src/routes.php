e1919ec7f1b35ff14aa87f0467c63772e1d7ccf6
Nerdy Naga <mozhui.lungdsuo@gmail.com>
2025-02-04 11:48:42 +0530

    sas

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Mozhuilungdsuo\Impersonator\Controllers\ImpersonationController;
 Route::middleware('web')->group(function () {
     Route::get('/impersonate/{id}', [ImpersonationController::class, 'impersonateUser'])->name('impersonate.start');
-    Route::get('/impersonate/stop', [ImpersonationController::class, 'stopImpersonating'])->name('impersonate.stop');
+    Route::get('/stopimpersonate', [ImpersonationController::class, 'stopImpersonating'])->name('impersonate.stop');
 });
